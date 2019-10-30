c511de1bb61dfed79af18f1c64b00feac2ee60a0
jmetais2017 <julien.metais@eleves.ec-nantes.fr>
2019-10-30 10:56:48 -0400

    glouton et dynamique finis

--- TP2/backtracking.py
+++ TP2/backtracking.py
@@ -1,18 +1,16 @@
 import numpy as np
 import time
 
-depthLimit = 2
-
-def backtrack(tab, currentDepth):
+def backtrack_rec(tab, currentDepth):
     n = len(tab[0])
     if(n == 0):
         return 0
     else:
-        if(currentDepth < depthLimit):
+        if(currentDepth > 0):
             maxProfit = 0
             for i in range(1, n+1):
                 newTab = [tab[0][0: n - i], tab[1][0: n - i]]
-                profit = backtrack(newTab, currentDepth + 1) + int(tab[1][i-1])
+                profit = backtrack_rec(newTab, currentDepth - 1) + int(tab[1][i-1])
                 if(profit > maxProfit):
                     maxProfit = profit
             return maxProfit
@@ -20,16 +18,29 @@ def backtrack(tab, currentDepth):
             return int(tab[1][n-1])
 
 
+def backtrack(filename):
+    #Conversion des données dans un tableau à 2 lignes
+    data = np.loadtxt(filename, skiprows = 1)
+
+    #Mesure de la durée d'exécution
+    start = time.time()
+
+    #Choix de la profondeur de recherche maximale
+    profit = backtrack_rec(data, 2)
+
+    end = time.time()
+    runtime = end - start
+
+    return profit, runtime
+
+
 exSize = 4000
 exNum = 3
 
 #Exemplaire à lire
-fileName = "./exemplaires/" + str(exSize) + "-" + str(exNum) + ".txt"
+filename = "./exemplaires/" + str(exSize) + "-" + str(exNum) + ".txt"
 
-#Conversion des données dans un tableau à 2 lignes
-data = np.loadtxt(fileName, skiprows = 1)
+profit, runtime = backtrack(filename)
 
-print(backtrack(data, 0))
-
-# a = [1, 2, 3, 4, 5, 6]
-# print(a[0:4])
+print("\n\n\n\nTAILLE " + str(exSize) + " : profit = " + str(profit) + " ; temps = " + str(runtime))
+#print(solutions)
--- TP2/dynamique.py
+++ TP2/dynamique.py
@@ -2,28 +2,47 @@ import numpy as np
 import time
 
 
-def dynamique(exSize, exNum):
+def dynamique(filename):
+    #Lecture de la taille de l'exemplaire
+    file = open(filename,"r")
+    exSize = int(file.readline())
+
     #print("\n\n\n\n____", exSize, "____")
 
-    #Exemplaire à lire
-    fileName = "./exemplaires/" + str(exSize) + "-" + str(exNum) + ".txt"
-
     #Conversion des données dans un tableau à 2 lignes
-    data = np.loadtxt(fileName, skiprows = 1)
+    data = np.loadtxt(filename, skiprows = 1)
 
     #Mesure de la durée d'exécution
     start = time.time()
 
+    #r[j] est le profit optimal pour une taille d'exemplaire <= j
     r = [0]
+    r_aux = [[]]
 
     for j in range(1, exSize + 1):
-        r_aux = [int(data[1][i-1]) + r[j - i] for i in range(1, j + 1)]
-        r.append(max(r_aux))
+        r_aux.append([int(data[1][i-1]) + r[j - i] for i in range(1, j + 1)])
+        #On obtient r[j] en effectuant le choix optimal à partir des r[j-i]
+        r.append(max(r_aux[j]))
+
+    #La réponse au problème se trouve donc en r[exSize]
+    profit = r[exSize]
+
+    #Récupérons maintenant les choix de coupes successifs
+    currentSize = exSize
+
+    choice = 0
+    solution = [] #Coupes successives
+
+    while (currentSize > 0):
+        choice = (r_aux[currentSize]).index(max(r_aux[currentSize])) + 1
+        solution.append(choice)
+
+        currentSize -= choice
 
     end = time.time()
-    delta = end - start
+    runtime = end - start
 
-    return r[exSize], delta
+    return profit, runtime, solution
 
 
 
@@ -31,16 +50,22 @@ def dynamique(exSize, exNum):
 exSizes = [10, 100, 400, 1000, 4000, 10000] #, 20000, 30000, 40000]
 #Nombre d'exemplaires de chaque taille
 N = 3
+filename = ""
 
 #Traitement de tous les exemplaires
 for exSize in exSizes:
     meanProfit = 0
     meanTime = 0.0
+    solutions = []
     for exNum in range(1, N + 1):
-        profit, runtime = dynamique(exSize, exNum)
+        #Exemplaire à lire
+        filename = "./exemplaires/" + str(exSize) + "-" + str(exNum) + ".txt"
+        profit, runtime, solution = dynamique(filename)
         meanProfit = meanProfit + profit
         meanTime = meanTime + runtime
+        solutions.append(solution)
     meanProfit = meanProfit / N
     meanTime = meanTime / N
 
     print("\n\n\n\nTAILLE " + str(exSize) + " : profit moyen = " + str(meanProfit) + " ; temps moyen = " + str(meanTime))
+    print(solutions)
